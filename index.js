96244eba928a4beab736be5c67aff43737dc4d72
shohel-ranaC <akashshohel422@gmail.com>
2022-10-28 20:04:38 -0700

    node js practice local modul

---
+++ index.js
@@ -0,0 +1,9 @@
+// const studentInfo = require('./student');
+const {getName, getAge, cgpa} = require('./student');
+
+// console.log(studentInfo.getName());
+// console.log(studentInfo.getAge());
+// console.log(studentInfo.cgpa);
+console.log(getName());
+console.log(getAge());
+console.log(cgpa);
---
+++ student.js
@@ -0,0 +1,32 @@
+// Use Local Module..........................
+// const getName = ()=>{
+//     return 'Shohel Rana';
+// }
+
+
+// const getAge = ()=>{
+//     return '22';
+// }
+
+// const cgpa = 4.96; 
+exports.getName = ()=>{
+    return 'Shohel Rana';
+}
+
+
+exports.getAge = ()=>{
+    return '22';
+}
+
+exports.cgpa = 4.96; 
+// all exports single line............
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// all exports together.............
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
